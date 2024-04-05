e514fe14ad5a08982c3cc680ea0d6b2e094cd7cf
Avalon <axj30.11.04@gmail.com>
2024-04-05 19:10:16 +0100

    init

---
+++ ModEntry.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Locations;
+using System.Security.Cryptography;
+
+namespace OreDetector
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        /// 
+
+        public static ModEntry instance;
+
+        private MineShaft currentShaft;
+
+        private OreDetector detector;
+
+        private Texture2D springObjects;
+
+        public override void Entry(IModHelper helper)
+        {
+            instance = this;
+            detector = new();
+            helper.Events.Player.Warped += this.OnWarped;
+            helper.Events.World.ObjectListChanged += this.OnObjectListChanged;
+            helper.Events.Display.Rendered += this.OnRendered;
+            springObjects = Helper.GameContent.Load<Texture2D>("maps\\springobjects.png");
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        ///
+
+        private void OnWarped(object? sender, WarpedEventArgs e)
+        {
+            if (e.NewLocation is MineShaft shaft)
+            {
+                currentShaft = shaft;
+                detector.GetOreInCurrentShaft();
+                foreach(var item in detector.Ores)
+                {
+                    Monitor.Log($"Max {item.Key}: {item.Value.Count}");
+                }
+            }
+        }
+        private void OnObjectListChanged(object? sender, ObjectListChangedEventArgs e) 
+        {
+            if (e.Location != currentShaft)
+                return;
+
+            foreach (var item in e.Removed)
+            {
+                if (!detector.Ores.Keys.Contains(item.Value.DisplayName))
+                    continue;
+
+                detector.MinedOres[item.Value.DisplayName].Add(item.Value);
+                //break; ??
+            }
+            foreach (var item in detector.Ores)
+            {
+                Monitor.Log($"Mined {item.Key}: {detector.MinedOres[item.Key].Count} / {item.Value.Count}");
+            }
+        }
+        private void OnRendered(object? sender, RenderedEventArgs e)
+        {
+            if(!Context.IsWorldReady) 
+                return;
+
+            if (Game1.player.currentLocation != currentShaft)
+                return;
+
+            DrawOverlayAbovePlayer(Game1.spriteBatch);
+
+
+        }
+
+        private void DrawOverlayAbovePlayer(SpriteBatch batch)
+        {
+            if (Game1.activeClickableMenu != null)
+                return;
+
+            Farmer player = Game1.player;
+
+            float transparency = player.isMoving() ? 0.1f : 1.0f;
+            Vector2 position = new Vector2(player.Position.X - Game1.viewport.X, player.Position.Y - Game1.viewport.Y);
+
+            //batch.DrawString(Game1.dialogueFont, "Test", position, Color.White);
+            string result = "";
+            int text_offsetY = detector.Ores.Count - 1;
+
+            int padding = 0;
+            foreach (var item in detector.Ores)
+            {
+                string text = $"{item.Key}: {detector.MinedOres[item.Key].Count} / {item.Value.Count}\n";
+                if (text.Length > padding)
+                    padding = text.Length;
+                result += text;
+            }
+
+            batch.DrawString(Game1.dialogueFont, result, position + new Vector2(-4 * padding , -128 - Game1.dialogueFont.LineSpacing * text_offsetY), Color.White * transparency);
+
+        }
+    }
+}
---
+++ OreDetector.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Network;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OreDetector
+{
+    public class OreDetector
+    {
+        public Dictionary<string, List<StardewValley.Object>> Ores;
+
+        public Dictionary<string, List<StardewValley.Object>> MinedOres;
+
+        public OreDetector()
+        {
+        }
+        public void GetOreInCurrentShaft()
+        {
+            Ores = new Dictionary<string, List<StardewValley.Object>>();
+            MinedOres = new Dictionary<string, List<StardewValley.Object>>();
+
+            MineShaft currentShaft = (MineShaft)Game1.player.currentLocation;
+            OverlaidDictionary current_ores = currentShaft.Objects;
+            foreach (var ore in current_ores.Values)
+            {
+                //Debug.WriteLine($"{ore.Name} {ore.Category} {ore.ParentSheetIndex}");
+                if ((ore.Category == -999 && ore.Name != "Weeds") || ore.Category == -2 || (ore.Category == -9 && ore.Name == "Barrel"))
+                {
+                    if (!Ores.ContainsKey(ore.DisplayName))
+                    {
+                        Ores.Add(ore.DisplayName, new List<StardewValley.Object>());
+                        MinedOres.Add(ore.DisplayName, new List<StardewValley.Object>());
+                    }
+                    Ores[ore.DisplayName].Add(ore);
+                }
+            }
+        }
+    }
+}
