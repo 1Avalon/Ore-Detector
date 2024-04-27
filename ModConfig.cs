49a7d50ac6e397d22922c83737fc8d6d77663d2d
Avalon <axj30.11.04@gmail.com>
2024-04-27 20:46:24 +0100

    add keybind option for setting custom position

--- ModConfig.cs
+++ ModConfig.cs
@@ -4,23 +4,26 @@ using System.Linq;
 using Microsoft.Xna.Framework;
 using System.Text;
 using System.Threading.Tasks;
+using StardewModdingAPI;
 
 namespace OreDetector
 {
     public sealed class ModConfig
     {
-        public string PositionOption { get; set; } = "Above player";
+        public string PositionOption { get; set; } = I18n.OreDetector_Config_AbovePlayer();
 
         public bool arrowPointingToLadder = false;
 
-        public string arrowToLadderColor = "Red";
+        public string arrowToLadderColor = I18n.OreDetector_Config_Color_Red();
 
         public bool arrowPointingToHole = false;
 
-        public string arrowToHoleColor = "Blue";
+        public string arrowToHoleColor = I18n.OreDetector_Config_Color_Blue();
 
         public bool showOreName = true;
 
         public Vector2 customPosition = new Vector2(0, 0);
+
+        public SButton customPositionKeybind = SButton.P;
     }
 }
