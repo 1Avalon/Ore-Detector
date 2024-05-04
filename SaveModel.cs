8b732cfeec4963c8f01c1c1c056942cba73ce080
Avalon <axj30.11.04@gmail.com>
2024-05-04 13:07:04 +0100

    init

---
+++ SaveModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OreDetector
+{
+    public class SaveModel
+    {
+        public List<string> blacklistedNames = new List<string>();
+
+        public List<string> discoveredMaterials = new List<string>();
+
+        public List<string> discoveredmaterialsQualifiedIds = new List<string>();
+    }
+}
